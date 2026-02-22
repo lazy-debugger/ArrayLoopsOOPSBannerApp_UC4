d168eb7f57cf522d43423283eb309965c5ce5917
lazy-debugger <as0464@srmist.edu.in>
2026-02-22 22:26:52 +0530

    Added Java Program

---
+++ StringJoin.java
@@ -0,0 +1,14 @@
+public class StringJoin
+{
+public static void main(String args[])
+{
+System.out.println(String.join(" ", "   ***   ", "   ***   ", "*****", " *****"));
+System.out.println(String.join(" ", " **   ** ", " **   ** ", "*    *", "*"));
+System.out.println(String.join(" ", "**     **", "**     **", "*    *", "*"));
+System.out.println(String.join(" ", "**     **", "**     **", "*    *", "*"));
+System.out.println(String.join(" ", "**     **", "**     **", "*****", " *****"));
+System.out.println(String.join(" ", "**     **", "**     **", "*   ", "      *"));
+System.out.println(String.join(" ", " **   ** ", " **   ** ", "*   ", "      *"));
+System.out.println(String.join(" ", "   ***   ", "   ***   ", "*   ", "  *****"));
+}
+}
